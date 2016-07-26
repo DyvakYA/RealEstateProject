359b27bed056843df5405e8f192f7efa06eabb58
DyvakYA <dyvakyurii@gmail.com>
2016-07-26 21:02:31 +0300

    add filters

--- src/estate/FilterUtils.java
+++ src/estate/FilterUtils.java
@@ -1,26 +1,28 @@
 package estate;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class FilterUtils {
-    
-    static void getPrice( List<Realty> estates, int price){ 
-        System.out.println("Filter Price");
-        for (Realty i : estates){ 
-        if (i.getPrice() > price){ 
-              System.out.println(i.toString()); 
-          } 
+
+    public static List<Realty> filterLowerPrice(List<Realty> realties, int price){
+
+        List<Realty> results = new ArrayList<>();
+        for (Realty realty : realties){
+            if (realty.getPrice() < price){
+                results.add(realty);
+            }
         }
-        System.out.println("------------------");
-     } 
- 
- 
-    static void getStreet(List<Realty> estates, String street){ 
-        System.out.println("Filter Streets");
-         for (Realty i: estates){ 
-             if (i.getStreet().equals(street)) 
-                 System.out.println(i.toString()); 
-         } 
-         System.out.println("------------------"); 
-     }    
+        return results;
+    }
+
+    public static List<Realty> filterByStreet(List<Realty> realties, String street){
+
+        List<Realty> results = new ArrayList<>();
+        for (Realty realty: realties){
+            if (realty.getStreet().equals(street))
+                results.add(realty);
+        }
+        return results;
+    }
 }
